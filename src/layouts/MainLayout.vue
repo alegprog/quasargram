866024e75ce7c73935bdeabb036c215f3953825e
Sergio Gtz <serosgb@gmail.com>
2020-08-09 14:39:46 -0500

    Header - Styles

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,20 +1,13 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+   <q-header
+      class="bg-white text-grey-10"
+      bordered
+      >
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          Quasargram
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -57,6 +50,8 @@ export default {
 </script>
 
 <style lang="sass">
+  .q-toolbar__title
+    text-align: center
   .q-footer
     .q-tab__icon
       font-size: 30px
