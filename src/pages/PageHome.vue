77ba7729f448a90332390e1d32eee9a91af8cb24
Sergio Gtz <serosgb@gmail.com>
2020-08-15 09:11:40 -0500

    Create a list of posts - Post Header

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,26 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <h5>Home Page</h5>
+
+  <q-card class="card-post" flat bordered>
+    <q-item>
+      <q-item-section avatar>
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+        </q-avatar>
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label class="text-bold">@serosgb</q-item-label>
+        <q-item-label caption>
+          AH, Michigan
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <q-separator />
+
+  </q-card>
+
   </q-page>
 </template>
 
