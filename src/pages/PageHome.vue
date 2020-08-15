ae00b16578bf93141c6b004645d5af87a4ffca99
Sergio Gtz <serosgb@gmail.com>
2020-08-15 13:39:48 -0500

    Addd mini-profile fixed column for desktop

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,38 +1,59 @@
 <template>
   <q-page class="constrain q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post p-mb-md"
+          flat
+          bordered>
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
 
-  <q-card
-    v-for="post in posts"
-    :key="post.id"
-    class="card-post p-mb-md"
-    flat
-    bordered>
-    <q-item>
-      <q-item-section avatar>
-        <q-avatar>
-          <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-        </q-avatar>
-      </q-item-section>
+            <q-item-section>
+              <q-item-label class="text-bold">@serosgb</q-item-label>
+              <q-item-label caption>
+                {{ post.location }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <q-item-section>
-        <q-item-label class="text-bold">@serosgb</q-item-label>
-        <q-item-label caption>
-          {{ post.location }}
-        </q-item-label>
-      </q-item-section>
-    </q-item>
+          <q-separator />
 
-    <q-separator />
+          <q-img
+            :src="post.imageUrl"
+          />
+          <q-card-section>
+            <div >{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
+          </q-card-section>
 
-    <q-img
-      :src="post.imageUrl"
-    />
-    <q-card-section>
-      <div >{{ post.caption }}</div>
-      <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
-    </q-card-section>
+        </q-card>        
+      </div>
+      <div class="col-4">
+          <q-item class="fixed">
+            <q-item-section avatar>
+              <q-avatar size="48px">
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
 
-  </q-card>
+            <q-item-section>
+              <q-item-label class="text-bold">@serosgb</q-item-label>
+              <q-item-label caption>
+                Sergio Gtz
+              </q-item-label>
+            </q-item-section>
+          </q-item>        
+      </div>
+    </div>
+
+
 
   </q-page>
 </template>
