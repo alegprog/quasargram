4e673bffe6e2629ed145617f9907a7c3122eccf5
Sergio Gtz <serosgb@gmail.com>
2020-09-01 23:51:32 -0500

    Handle loading part 1

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -2,38 +2,43 @@
   <q-page class="constrain q-pa-md">
     <div class="row q-col-gutter-lg">
       <div class="col-12 col-sm-8">
-        <q-card
-          v-for="post in posts"
-          :key="post.id"
-          class="card-post p-mb-md"
-          flat
-          bordered>
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-              </q-avatar>
-            </q-item-section>
+        <template v-if="!loadingPosts">
+          <q-card
+            v-for="post in posts"
+            :key="post.id"
+            class="card-post p-mb-md"
+            flat
+            bordered>
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                </q-avatar>
+              </q-item-section>
 
-            <q-item-section>
-              <q-item-label class="text-bold">@serosgb</q-item-label>
-              <q-item-label caption>
-                {{ post.location }}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
+              <q-item-section>
+                <q-item-label class="text-bold">@serosgb</q-item-label>
+                <q-item-label caption>
+                  {{ post.location }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
 
-          <q-separator />
+            <q-separator />
 
-          <q-img
-            :src="post.imageUrl"
-          />
-          <q-card-section>
-            <div >{{ post.caption }}</div>
-            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
-          </q-card-section>
+            <q-img
+              :src="post.imageUrl"
+            />
+            <q-card-section>
+              <div >{{ post.caption }}</div>
+              <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
+            </q-card-section>
 
-        </q-card>        
+          </q-card>
+        </template>
+        <template v-else>
+          Loading...
+        </template>  
       </div>
       <div class="col-4 large-screen-only">
           <q-item class="fixed">
@@ -65,7 +70,8 @@ export default {
   name: 'PageHome',
   data() {
     return {
-      posts: []
+      posts: [],
+      loadingPosts: false
     }
   },
   methods: {
