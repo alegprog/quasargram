e40ba53afedbefe5780d47fcde0ed8c840653daa
Sergio Gtz <serosgb@gmail.com>
2020-09-20 23:24:31 -0500

    Store field data as a Post

--- backend/app.js
+++ backend/app.js
@@ -58,6 +58,7 @@
   app.post('/posts', (request, response) => {
     response.set('Access-Control-Allow-Origin', '*')
     let busboy = new Busboy({ headers: request.headers });
+    let fields = {};
 
     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
       console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
@@ -71,11 +72,18 @@
 
     busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
       console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+      fields[fieldname] = val;
     });
 
     busboy.on('finish', function() {
-      console.log('Done parsing form!');
-      // response.writeHead(303, { Connection: 'close', Location: '/' });
+      db.collection('posts').doc(fields.id).set({
+        id: fields.id,
+        caption: fields.caption,
+        location: fields.location,
+        date: parseInt(fields.date),
+        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/quasargram-cb983.appspot.com/o/golden_gate.jpg?alt=media&token=28d592e1-8091-4dab-9ab0-62750b96858d'
+      });
+
       response.send('Done parsing form!');
     });
 
