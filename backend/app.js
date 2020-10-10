ccf3fcd2cbefa65bda8d7048e7a9aa7ffdfedda6
Sergio Gtz <serosgb@gmail.com>
2020-10-10 09:11:19 -0500

    Upload complete Upload image to cloud storage and storage the image URL

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,13 @@
  */
 
   const express = require('express')
-  const admin = require('firebase-admin');
-  let inspect = require('util').inspect;
-  let Busboy =  require('busboy');
+  const admin = require('firebase-admin')
+  let inspect = require('util').inspect
+  let Busboy =  require('busboy')
+  let path = require('path')
+  let os = require('os')
+  let fs = require('fs')
+  let UUID = require('uuid-v4')
 
   /*
   config - express
@@ -58,9 +62,11 @@
   // Create post
 
   app.post('/posts', (request, response) => {
-    response.set('Access-Control-Allow-Origin', '*')
+    response.set('Access-Control-Allow-Origin', '*');
+    let uuid = UUID();
     let busboy = new Busboy({ headers: request.headers });
     let fields = {};
+    let fileData = {};
 
     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
       console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
@@ -70,6 +76,12 @@
       file.on('end', function() {
         console.log('File [' + fieldname + '] Finished');
       });
+
+      // Saving image in the temporary folder
+      // /tmp/56465465-41645645.png
+      let filepath = path.join(os.tmpdir(), filename)
+      file.pipe(fs.createWriteStream(filepath))
+      fileData = { filepath, mimetype }
     });
 
     busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
@@ -78,15 +90,36 @@
     });
 
     busboy.on('finish', function() {
-      db.collection('posts').doc(fields.id).set({
-        id: fields.id,
-        caption: fields.caption,
-        location: fields.location,
-        date: parseInt(fields.date),
-        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/quasargram-cb983.appspot.com/o/golden_gate.jpg?alt=media&token=28d592e1-8091-4dab-9ab0-62750b96858d'
-      });
+      bucket.upload(
+        fileData.filepath,
+        {
+          uploadType: 'media',
+          metadata: {
+            metadata: {
+              contentType: fileData.mimetype,
+              firebaseStorageDownloadTokens: uuid
+            }
+          }
+        },
+        (error, uploadedFile) => {
+          if (!error) {
+            createDocument(uploadedFile)
+          }
+        }
+      )
 
-      response.send('Done parsing form!');
+      function createDocument(uploadedFile) {
+        db.collection('posts').doc(fields.id).set({
+          id: fields.id,
+          caption: fields.caption,
+          location: fields.location,
+          date: parseInt(fields.date),
+          imageUrl: `https://firebasestorage.googleapis.com/v0/b/${ bucket.name }/o/${ uploadedFile.name }?alt=media&token=${ uuid }`
+        }).then(() => {
+          response.send('Post added: ' + fields.id)
+        });
+      }
+
     });
 
     request.pipe(busboy);
